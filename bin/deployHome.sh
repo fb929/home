0d2ad6d769b5372abb9f93bc866e5400fbe59ec3
Grigory Efimov <g.efimov@corp.mail.ru>
2018-04-28 19:21:28 +0300

    fix README.md add bin/deployHome.sh

--- bin/deployHome.sh
+++ bin/deployHome.sh
@@ -1,7 +1,7 @@
-# home
-deploy:  
-```
-VERSION='0.1'
+#!/bin/bash
+
+VERSION=${1:-"0.1"}
+
 install -d $HOME/tmp/ &&
 if [[ -d $HOME/tmp/home-$VERSION ]]; then
 	rm -rf $HOME/tmp/home-$VERSION
@@ -10,6 +10,3 @@ wget --quiet --output-document=$HOME/tmp/v$VERSION.tar.gz https://github.com/fb9
 tar --gzip --extract --directory=$HOME/tmp/ --exclude=README.md --file=$HOME/tmp/v$VERSION.tar.gz &&
 rsync -a $HOME/tmp/home-$VERSION/ $HOME/
 echo $VERSION > $HOME/.home_version
-```
-
-or run bin/deployHome.sh
