305735dc9489107bfc9b011047ace6dbfbeb2fba
Grigory Efimov <grigory.efimov@gmail.com>
2023-08-08 19:59:23 +0300

    added .gitconfig.gpg

--- bin/fixPerm.sh
+++ bin/fixPerm.sh
@@ -27,3 +27,8 @@ chmod -R 0750 $HOME/bin/
 if [[ -f $HOME/.bash_history ]]; then
     chown $USER:$GROUP $HOME/.bash_history
 fi
+
+# gitconfig
+if [[ -d $HOME/gpg/ ]]; then
+    mv $HOME/.gitconfig.gpg $HOME/.gitconfig
+fi
